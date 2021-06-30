7cbd83b1a5d2d2f527efb86843a36ea16426a455
Gaurav Lathwal <gauravlathwal@Gauravs-MacBook-Pro.local>
2021-06-30 16:34:37 +0530

    adding bare-bone main file for aws & gcp

---
+++ aws/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  engine            = "postgres"
+  engine_version    = "12.5"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  name     = "demodb"
+  username = "postgres_user"
+  password = "master_password"
+
+  storage_type = "gp2"
+  multi_az = "false"
+}
+
